d226279b65466790da805ebca5cc30bb1763a8a8
bachhovuong310 <haimeo94@gmail.com>
2016-04-02 09:31:51 +0700

    Delete UserOperations.java

--- HelloWorld/src/com/helloworld/order/db/UserOperations.java
+++
@@ -1,9 +0,0 @@
-package com.helloworld.order.db;
-
-import com.helloworld.order.User;
-
-public interface UserOperations {
-	User findOneByUsername(String username);
-	void deleteUserByUsername(String username);
-
-}
